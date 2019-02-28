9d0f49fd9864033e37af996c2b4601b234741e9b
Jonquil <ok18847@bristol.ac.uk>
2019-02-28 14:21:15 +0000

    initial version

---
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -0,0 +1,112 @@
+package uk.ac.bris.cs.scotlandyard.model;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.DOUBLE;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.SECRET;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
+
+// TODO implement all methods and pass all tests
+public class ScotlandYardModel implements ScotlandYardGame {
+
+	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
+			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
+			PlayerConfiguration... restOfTheDetectives) {
+		// TODO
+	}
+
+	@Override
+	public void registerSpectator(Spectator spectator) {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public void unregisterSpectator(Spectator spectator) {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public void startRotate() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Collection<Spectator> getSpectators() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public List<Colour> getPlayers() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Set<Colour> getWinningPlayers() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Optional<Integer> getPlayerLocation(Colour colour) {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Optional<Integer> getPlayerTickets(Colour colour, Ticket ticket) {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public boolean isGameOver() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Colour getCurrentPlayer() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public int getCurrentRound() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public List<Boolean> getRounds() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+	@Override
+	public Graph<Integer, Transport> getGraph() {
+		// TODO
+		throw new RuntimeException("Implement me");
+	}
+
+}
